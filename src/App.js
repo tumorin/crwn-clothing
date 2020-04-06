6cd4b8fe0b2fd253ad36be40748411ffc7f7dfd5
Yurii Tumorin <ytumorin@n-ix.com>
2020-04-06 20:39:33 +0300

    implement firebase util

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor () {
@@ -20,9 +20,24 @@ class App extends React.Component {
   unsubscibeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscibeFromAuth = auth.onAuthStateChanged(user=>{
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unsubscibeFromAuth = auth.onAuthStateChanged( async userAuth=>{
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        console.log(userRef)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state);
+        })
+      } else {
+        this.setState ({
+          currentUser: userAuth
+        })
+      }
     })
   } 
 
